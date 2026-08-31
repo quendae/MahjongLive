49cb5de558a92b08333fd03afe0f5900640960ad
quendae <qnd.000@gmail.com>
2026-08-31 11:15:20 +0200

    fix(client): type persistent 3D stage as required

--- client/src/dom-query.d.ts
+++ client/src/dom-query.d.ts
@@ -2,6 +2,7 @@ export {};
 
 declare global {
   interface Document {
+    querySelector<E extends Element = HTMLDivElement>(selectors: '#table-3d-stage'): E;
     querySelector<E extends Element = Element>(selectors: string): E | null;
   }
 }
